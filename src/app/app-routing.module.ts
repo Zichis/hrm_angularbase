5109219e800e2b802e0058eecdd1360c320ac9fc
Ezichi Ebere <tripleee@yahoo.co.uk>
2020-11-20 19:58:26 +0100

    Added routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {'path': '', component: HomeComponent},
+  {'path': 'welcome', component: WelcomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
