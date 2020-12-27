688174d7e0283e5277a20a395204f0a72e1f865d
Ezichi Ebere <tripleee@yahoo.co.uk>
2020-12-27 23:12:50 +0100

    Created login component.

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { LoginService } from '../services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    email: [''],
+    password: ['']
+  });
+
+  constructor(private fb: FormBuilder, private loginService: LoginService) { }
+
+  ngOnInit(): void {
+  }
+
+  onLogin(): void {
+    console.log(this.loginForm.value);
+    this.loginService.loginUser(this.loginForm.value).subscribe((data) => {
+      console.log(data);
+      //console.log(data.token.original.token);
+      localStorage.setItem('baseAppToken', data['token']['original']['token']);
+    });
+  }
+
+}
