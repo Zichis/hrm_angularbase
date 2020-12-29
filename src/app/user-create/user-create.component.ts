06be0513a0dfe4613ad37eaf7f089d68971703b0
Ezichi Ebere <tripleee@yahoo.co.uk>
2020-12-29 23:53:02 +0100

    User create component form added.

--- src/app/user-create/user-create.component.ts
+++ src/app/user-create/user-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { UsersService } from '../services/users.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-user-create',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-create.component.scss']
 })
 export class UserCreateComponent implements OnInit {
+  createForm = this.fb.group({
+    email: '',
+    password: '',
+    first_name: '',
+    last_name: ''
+  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private usersService: UsersService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onCreate(): void {
+    console.log(this.createForm.value);
+    this.usersService.createUser(this.createForm.value).subscribe((data) => {
+      console.log(data);
+      this.router.navigate(['/']);
+    }, (error) => {
+        //
+      }
+    );
   }
 
 }
