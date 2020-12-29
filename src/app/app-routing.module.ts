cd51ae4374827ae15bdc074c92240d5674bbe2cf
Ezichi Ebere <tripleee@yahoo.co.uk>
2020-12-29 09:11:33 +0100

    Added user create route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent} from './login/login.component';
+import { UserCreateComponent } from './user-create/user-create.component';
 
 const routes: Routes = [
   {'path': '', component: HomeComponent},
   {'path': 'welcome', component: WelcomeComponent},
-  {'path': 'login', component: LoginComponent}
+  {'path': 'login', component: LoginComponent},
+  {'path': 'users/create', component: UserCreateComponent}
 ];
 
 @NgModule({
