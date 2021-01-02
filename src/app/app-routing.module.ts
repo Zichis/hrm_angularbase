240def700462ecff702e78cf88b93cb18acb1f85
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-01-02 21:00:11 +0100

    Route created for user update.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent} from './login/login.component';
 import { UserCreateComponent } from './user-create/user-create.component';
 import { UserShowComponent } from './user-show/user-show.component';
+import { UserUpdateComponent } from "./user-update/user-update.component";
 
 const routes: Routes = [
   {'path': '', component: HomeComponent},
   {'path': 'welcome', component: WelcomeComponent},
   {'path': 'login', component: LoginComponent},
   {'path': 'users/create', component: UserCreateComponent},
-  {'path': 'users/:id', component: UserShowComponent}
+  {'path': 'users/:id', component: UserShowComponent},
+  {'path': 'users/:id/edit', component: UserUpdateComponent}
 ];
 
 @NgModule({
