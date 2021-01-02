fdaafb7ad7cf1a4555a1b87ab10d5f638279497b
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-01-02 22:29:58 +0100

    Add form in update page.

--- src/app/user-update/user-update.component.ts
+++ src/app/user-update/user-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-user-update',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-update.component.scss']
 })
 export class UserUpdateComponent implements OnInit {
+  user: any;
+  updateForm = this.fb.group({
+    first_name: '',
+    last_name: ''
+  });
 
-  constructor() { }
+  constructor(
+    private usersService: UsersService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.usersService.getUser(id).subscribe((data) => {
+      this.user = data;
+      this.updateForm.patchValue({
+        first_name: data['first_name'],
+        last_name: data['last_name']
+      });
+    }, (error) => {
+        //
+      }
+    );
+  }
+
+  onUpdate(): void {
+    console.log(this.updateForm.value);
+    this.usersService.updateUser(this.updateForm.value, this.user['id']).subscribe((data) => {
+      this.router.navigate(['/']);
+    }, (error) => {
+        //
+      }
+    );
   }
 
 }
