316181bbe6f2164dc0e998a258ec82fd8c71a57a
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-01-03 20:23:54 +0100

    Added auth guard to routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import { LoginComponent} from './login/login.component';
 import { UserCreateComponent } from './user-create/user-create.component';
 import { UserShowComponent } from './user-show/user-show.component';
 import { UserUpdateComponent } from "./user-update/user-update.component";
+import { AuthGuard } from "./auth.guard";
 
 const routes: Routes = [
-  {'path': '', component: HomeComponent},
-  {'path': 'welcome', component: WelcomeComponent},
+  {'path': '', component: HomeComponent, canActivate: [AuthGuard]},
+  {'path': 'welcome', component: WelcomeComponent, canActivate: [AuthGuard]},
   {'path': 'login', component: LoginComponent},
-  {'path': 'users/create', component: UserCreateComponent},
-  {'path': 'users/:id', component: UserShowComponent},
-  {'path': 'users/:id/edit', component: UserUpdateComponent}
+  {'path': 'users/create', component: UserCreateComponent, canActivate: [AuthGuard]},
+  {'path': 'users/:id', component: UserShowComponent, canActivate: [AuthGuard]},
+  {'path': 'users/:id/edit', component: UserUpdateComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
