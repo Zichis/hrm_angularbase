d5ec3dbbc9f158861197ba01d9cf509277ace9d9
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-01-18 21:16:20 +0100

    Added user table in user component.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users.service';
+import { faUserPlus, faEdit, faTrash, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  users: Object;
+  edit = faEdit;
+  trash = faTrash;
+  userPlus = faUserPlus;
+  infoCircle = faInfoCircle;
+  user: Object;
+  deleteClicked: boolean = false;
 
-  constructor() { }
+  constructor(private userService: UsersService) { }
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe((users) => {
+      this.users = users;
+      console.log(this.users);
+    }, (error) => {
+      if (error.status === 401) {
+        window.location.href = "/login";
+      }
+    });
+  }
+
+  setInitial(user) {
+    return user.first_name.charAt(0).toUpperCase();
+  }
+
+  onDelete(id: any) {
+    this.deleteClicked = true;
+    this.userService.getUser(id).subscribe((data) => {
+      this.user = data;
+    }, (_error) => {
+        //
+      }
+    );
+  }
+
+  onCancelDelete() {
+    this.deleteClicked = false;
+  }
+
+  onConfirmDelete(id: any) {
+    this.userService.deleteUser(id).subscribe((response) => {
+      this.users = response['data']['users'];
+      this.deleteClicked = false;
+    }, (error) => {
+      if (error.status === 401) {
+        window.location.href = "/login";
+      }
+    })
   }
 
 }
