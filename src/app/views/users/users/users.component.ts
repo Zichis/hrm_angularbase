8cfc5116af135731eb3235a0ce518deffd2cb58c
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-02-11 13:12:05 +0100

    Sign user out on delete if needed.

--- src/app/views/users/users/users.component.ts
+++ src/app/views/users/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../../services/users.service';
+import { User } from '../../../models/user';
 import { faUserPlus, faEdit, faTrash, faInfoCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -8,20 +9,21 @@ import { faUserPlus, faEdit, faTrash, faInfoCircle, faTimesCircle } from '@forta
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-  users: Object;
+  users: User[];
   edit = faEdit;
   trash = faTrash;
   userPlus = faUserPlus;
   infoCircle = faInfoCircle;
   timesCircle = faTimesCircle;
-  user: Object;
+  user: User;
+  loggedUser: User;
   deleteClicked: boolean = false;
   alertMsg = localStorage.getItem('baseAppAlert');
 
   constructor(private userService: UsersService) { }
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe((users) => {
+    this.userService.getUsers().subscribe((users: User[]) => {
       this.users = users;
       this.autoRemoveAlert();
       console.log(this.users);
@@ -29,6 +31,10 @@ export class UsersComponent implements OnInit {
       if (error.status === 401) {
         window.location.href = "/login";
       }
+    });
+
+    this.userService.getCurrentUser().subscribe((response) => {
+      this.loggedUser = response['data']['user'];
     });
   }
 
@@ -38,7 +44,7 @@ export class UsersComponent implements OnInit {
 
   onDelete(id: any) {
     this.deleteClicked = true;
-    this.userService.getUser(id).subscribe((data) => {
+    this.userService.getUser(id).subscribe((data: User) => {
       this.user = data;
     }, (_error) => {
         //
@@ -53,15 +59,19 @@ export class UsersComponent implements OnInit {
   onConfirmDelete(id: any) {
     this.userService.deleteUser(id).subscribe((response) => {
       this.users = response['data']['users'];
-      this.deleteClicked = false;
       localStorage.setItem('baseAppAlert', `Account deleted.`);
       this.alertMsg = localStorage.getItem('baseAppAlert');
-      // Redirect to onboarding if user deleted is last user
-      this.userService.numberOfUsers().subscribe((response) => {
-        if (response['data']['users'] < 1) {
-          window.location.href = "/onboarding";
-        }
-      });
+
+      // If user deleted is the last user
+      if (this.users.length < 1) {
+        this.signOut();
+      }
+
+      // If user deleted is the logged user
+      if (this.loggedUser.id == id) {
+        this.signOut('/login');
+      }
+     
     }, (error) => {
       if (error.status === 401) {
         window.location.href = "/login";
@@ -80,6 +90,14 @@ export class UsersComponent implements OnInit {
     console.log("We are ready!");
     localStorage.removeItem("baseAppAlert");
     this.alertMsg = null;
+  }
+
+  signOut(redirectUrl:string = "/onboarding") {
+    this.userService.signOut().subscribe((response) => {
+      localStorage.removeItem('baseAppToken');
+      localStorage.removeItem("baseAppAlert");
+      window.location.href = redirectUrl;
+    });
   }
 
 }
