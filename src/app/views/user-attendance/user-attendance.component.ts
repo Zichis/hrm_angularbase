9282ccb0c16e3561244d5e763cb6ad4b0fbcf47d
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-03-01 19:03:44 +0100

    Displayed user's attendances in table.

--- src/app/views/user-attendance/user-attendance.component.ts
+++ src/app/views/user-attendance/user-attendance.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AttendanceService } from 'src/app/services/attendance.service';
+import { Attendance } from '../../models/attendance.model';
 
 @Component({
   selector: 'app-user-attendance',
@@ -9,10 +10,15 @@ import { AttendanceService } from 'src/app/services/attendance.service';
 export class UserAttendanceComponent implements OnInit {
   canClockIn: boolean = true;
   canClockOut: boolean = true;
+  attendances: Attendance[];
 
   constructor(private attendanceService: AttendanceService) { }
 
   ngOnInit(): void {
+    this.attendanceService.getAll().subscribe((data: Attendance[]) => {
+      this.attendances = data;
+    });
+
     this.attendanceService.status().subscribe((data) => {
       console.log(data);
       this.canClockIn = data['canClockIn'];
