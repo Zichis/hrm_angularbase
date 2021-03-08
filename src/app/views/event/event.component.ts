53157e612541ae0fa20d8844a2765bc14dbf7459
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-03-08 10:42:00 +0100

    Added basic full calendar options.

--- src/app/views/event/event.component.ts
+++ src/app/views/event/event.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-event',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event.component.scss']
 })
 export class EventComponent implements OnInit {
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    events: [
+      { title: 'Home Coming', date: '2021-03-08' },
+      { title: 'Project Launch', date: '2021-03-09' }
+    ]
+  };
 
   constructor() { }
 
