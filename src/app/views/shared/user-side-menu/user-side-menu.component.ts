50070a6dcc1402887b7ca256582ba33706f3b972
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-03-09 16:58:15 +0100

    Updated user-side-menu view and styles.

--- src/app/views/shared/user-side-menu/user-side-menu.component.ts
+++ src/app/views/shared/user-side-menu/user-side-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { faBars, faCalendarCheck, faCog, faTh, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-user-side-menu',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-side-menu.component.scss']
 })
 export class UserSideMenuComponent implements OnInit {
+  @Input() sideMenuOpen: boolean;
+  th = faTh;
+  userIcon = faUser;
+  menuIcon = faBars;
+  settingIcon = faCog;
+  attendanceIcon = faCalendarCheck;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCloseSideMenu() {
+    this.sideMenuOpen = false;
   }
 
 }
