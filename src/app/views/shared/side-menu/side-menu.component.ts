82447d08b0a907ca07e0f8faa9bbdf70c32436e0
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-03-09 16:34:24 +0100

    Updated side-menu component, styles.

--- src/app/views/shared/side-menu/side-menu.component.ts
+++ src/app/views/shared/side-menu/side-menu.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { faBuffer } from '@fortawesome/free-brands-svg-icons';
+import { faBuilding, faCalendarCheck, faCog, faTh, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-side-menu',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-menu.component.scss']
 })
 export class SideMenuComponent implements OnInit {
+  @Input() sideMenuOpen: boolean;
+  th = faTh;
+  usersIcon = faUsers;
+  attendanceIcon = faCalendarCheck;
+  bufferIcon = faBuffer;
+  departmentIcon = faBuilding;
+  settingIcon = faCog;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCloseSideMenu() {
+    this.sideMenuOpen = false;
   }
 
 }
