949a086d2281270764b2d06601b0ff6522302331
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-03-09 20:28:44 +0100

    Added getDepartments method to service.

--- src/app/services/department.service.ts
+++ src/app/services/department.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
+  authToken = localStorage.getItem('baseAppToken');
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${this.authToken}`
+  });
+  baseUrl: string = 'http://hrm_lumenbase.test';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getDepartments(){
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.authToken}`
+    });
+    return this.http.get(`${this.baseUrl}/admin/departments`, {'headers': headers});
+  }
 }
