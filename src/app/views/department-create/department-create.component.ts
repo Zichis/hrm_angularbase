33abecdff0de0aabc75aa182284af9d4e7984343
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-03-10 16:14:17 +0100

    Display form to create department with validation.

--- src/app/views/department-create/department-create.component.ts
+++ src/app/views/department-create/department-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DepartmentService } from 'src/app/services/department.service';
 
 @Component({
   selector: 'app-department-create',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./department-create.component.scss']
 })
 export class DepartmentCreateComponent implements OnInit {
+  createDepartmentForm = this.fb.group({
+    name: ['', [Validators.required]]
+  });
+  nameError = '';
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private departmentService: DepartmentService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  get name(){ return this.createDepartmentForm.get('name'); }
+
+  onCreate(): void {
+    console.log(this.createDepartmentForm.value);
+    this.departmentService.createDepartment(this.createDepartmentForm.value).subscribe((data) => {
+      //localStorage.setItem('baseAppAlert', `New department created!`);
+      this.router.navigate(['/admin/departments']);
+    }, (error) => {
+        // Handle error!
+      }
+    );
   }
 
 }
