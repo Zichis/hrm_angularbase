21212d8a95759b06139361d5fe0584f91e33dbef
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-03-11 13:14:56 +0100

    Updated view for department show.

--- src/app/views/department-show/department-show.component.ts
+++ src/app/views/department-show/department-show.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { DepartmentService } from 'src/app/services/department.service';
+import { Department } from '../../models/department.model';
 
 @Component({
   selector: 'app-department-show',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./department-show.component.scss']
 })
 export class DepartmentShowComponent implements OnInit {
+  backArrowIcon = faChevronLeft;
+  department: Department;
 
-  constructor() { }
+  constructor(
+    private departmentService: DepartmentService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.departmentService.getDepartment(id).subscribe((data) => {
+      console.log(data['department']);
+      this.department = data['department'];
+    }, (error) => {
+        // Handle error!
+      }
+    );
   }
 
 }
