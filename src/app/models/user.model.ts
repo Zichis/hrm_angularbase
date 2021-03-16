e056c16952d9c9c23c1c1c82be63177bbe511f93
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-03-16 09:15:44 +0100

    Imported role model into user model.

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,8 +1,12 @@
+import { Role } from './role.model';
+
 export interface User {
     id: string;
     email: string;
+    department_id: number;
     personal : {
       first_name: string;
       last_name: string;
-    }
+    },
+    roles : Role[]
 }
