78377f7b34b9d896f10bfcc9720c8d7ae3556afe
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-03-28 23:41:39 +0100

    Designed dashboard with chart.

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faBuffer } from '@fortawesome/free-brands-svg-icons';
 import { faBuilding, faUsers } from '@fortawesome/free-solid-svg-icons';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,6 +13,27 @@ export class DashboardComponent implements OnInit {
   usersIcon = faUsers;
   departmentIcon = faBuilding;
   eventIcon = faBuffer;
+
+  lineChartData: ChartDataSets[] = [
+    { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
+  ];
+
+  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+
+  lineChartOptions = {
+    responsive: true,
+  };
+
+  lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,255,0,0.28)',
+    },
+  ];
+
+  lineChartLegend = true;
+  lineChartPlugins = [];
+  lineChartType = 'line';
 
   constructor() { }
 
