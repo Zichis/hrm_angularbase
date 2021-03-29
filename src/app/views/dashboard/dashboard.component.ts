9b33526cb9331d09095b3477b5de23efc4c9a746
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-03-29 23:27:44 +0100

    Displayed the number of users in dashboard card

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { faBuffer } from '@fortawesome/free-brands-svg-icons';
 import { faBuilding, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
+import { User } from 'src/app/models/user.model';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,6 +15,8 @@ export class DashboardComponent implements OnInit {
   usersIcon = faUsers;
   departmentIcon = faBuilding;
   eventIcon = faBuffer;
+  users: User[];
+  numberOfUsers: number;
 
   lineChartData: ChartDataSets[] = [
     { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
@@ -35,10 +39,12 @@ export class DashboardComponent implements OnInit {
   lineChartPlugins = [];
   lineChartType = 'line';
 
-  constructor() { }
+  constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
-    //
+    this.usersService.numberOfUsers().subscribe((response: {data: {users: number}}) => {
+      this.numberOfUsers = response.data.users;
+    });
   }
 
 }
